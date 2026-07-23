e768b510b2f3b615ef23a16f0add56c1ed8c5293
digio <40724487+gioblog@users.noreply.github.com>
2026-07-23 14:58:05 -0700

    fridge refactored

--- Cafe Cupid/Assets/Scripts/Fridge.cs
+++ Cafe Cupid/Assets/Scripts/Fridge.cs
@@ -10,10 +10,50 @@ public class Fridge : MonoBehaviour
 
     private SpriteRenderer[] ingredientSpriteComponents;
     private SpriteRenderer[] organizersSpriteComponents;
-    //private void OnMouseDown()
-    //{
-    //    Debug.Log("mouse clicked");
-    //    Collider2D colliderSelected= this.GetComponentInChildren<Collider2D>();
-    //    Debug.Log(colliderSelected); 
-    //}
+ 
+    public void Handle()
+    {
+        Debug.Log("collision entered with " + handle.name);
+        SpriteRenderer handleSprite = handle.GetComponent<SpriteRenderer>();
+
+        handleSprite.enabled = false; //turn off the sprite renderer 
+        interiorDoor.GetComponent<SpriteRenderer>().enabled = true;
+
+        for (int i = 0; i < ingredients.Count; i++) //make ingredients in the fridge visible
+        {
+            ingredientSpriteComponents = ingredients[i].GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer sprite in ingredientSpriteComponents)
+            {
+                sprite.enabled = true;
+            }
+        }
+
+        organizersSpriteComponents = organizers.GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer sprite in organizersSpriteComponents)  //make shelving visible 
+        {
+            sprite.enabled = true;
+        }
+    }
+
+    public void CloseDoor()
+    {
+        Debug.Log("collision entered with " + interiorDoor.name);
+        SpriteRenderer DoorSprite = interiorDoor.GetComponent<SpriteRenderer>();
+        DoorSprite.enabled = false;
+        handle.GetComponent<SpriteRenderer>().enabled = true;
+        for (int i = 0; i < ingredients.Count; i++) //make ingredients in the fridge invisible
+        {
+            ingredientSpriteComponents = ingredients[i].GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer sprite in ingredientSpriteComponents)
+            {
+                sprite.enabled = false;
+            }
+        }
+
+        organizersSpriteComponents = organizers.GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer sprite in organizersSpriteComponents) //make shelving invisible 
+        {
+            sprite.enabled = false;
+        }
+    }
 }
--- Cafe Cupid/Assets/Scripts/Player.cs
+++ Cafe Cupid/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     [SerializeField] private Camera mainCamera;
     [SerializeField] private float speed;
     [SerializeField] LayerMask defaultLayer;
+    [SerializeField] Fridge fridgeScriptReference; 
 
     private Vector2 position;
     private Vector2 velocity; 
@@ -18,19 +19,38 @@ public class Player : MonoBehaviour
         position = mainCamera.transform.position;
         velocity = speed * direction.x * mainCamera.transform.right; //right = red axis (x)  
         position += velocity * Time.fixedDeltaTime;
+        //if(position.x < -8)
+        //{
+        //    position.x = -6; 
+        //}
         mainCamera.transform.position = position; //updates where the camera is? 
     }
 
-    public void Move(InputAction.CallbackContext content)
+    public void Move(InputAction.CallbackContext context)
     {
-       direction = content.ReadValue<Vector2>();
+       direction = context.ReadValue<Vector2>();
        direction = direction.normalized;
     }
 
     //trying to get interaction input system to work so that I can develop for controller & mouse
-    public void Interact(InputAction.CallbackContext content)
+    public void Interact(InputAction.CallbackContext context)
     {
-       
+        if (!context.started) return;  //only want initial click
+
+        //cast a ray
+        var rayHit = Physics2D.GetRayIntersection(mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        
+        if (!rayHit.collider) return;  //no collider found 
+
+        switch(rayHit.collider.gameObject.name)
+        {
+            case "Handle":
+                fridgeScriptReference.Handle(); 
+                break;
+            case "Door(interior)":
+                fridgeScriptReference.CloseDoor(); 
+                break;
+        }
     }
 
 }
