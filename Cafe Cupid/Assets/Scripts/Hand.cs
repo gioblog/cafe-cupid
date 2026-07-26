93e3cf859a8a2dae7348e959594a20f7bf812da6
digio <40724487+gioblog@users.noreply.github.com>
2026-07-25 17:46:48 -0700

    Update Hand.cs

--- Cafe Cupid/Assets/Scripts/Hand.cs
+++ Cafe Cupid/Assets/Scripts/Hand.cs
@@ -5,11 +5,7 @@ public class Hand : MonoBehaviour
 {
     [SerializeField] GameObject item;
     [SerializeField] Sprite mug;  
-    //private void Awake()
-    //{
-    //    //Assets 
-    //    mug = Resources.Load<Sprite>("Assets/Resources/mug_sketch.png");
-    //}
+    
     public void HoldCup(GameObject cup)
     {
         Debug.Log("lets attempt to display"); 
